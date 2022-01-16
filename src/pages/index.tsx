0ad26987844a846212fb3c31cc2de366cf094999
Victor Garcia Mestre <vgmestre@gmail.com>
2022-01-16 21:41:38 +0100

    fetch random pokemon numbers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { trpc } from '@/utils/trpc';
+import { getOptionsForVote } from '@/utils/getRandomPokemon';
 
 const Home: NextPage = () => {
+  const [first, second] = getOptionsForVote();
   return (
     <div>
       <Head>
@@ -13,9 +16,9 @@ const Home: NextPage = () => {
         <div className="text-2xl text-center">Which Pokemon is rounder?</div>
         <div className="p-2" />
         <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-          <div className="w-16 h-16 bg-red-200" />
+          <div className="w-16 h-16 bg-red-800">{first}</div>
           <div className="p-8">Vs</div>
-          <div className="w-16 h-16 bg-red-200" />
+          <div className="w-16 h-16 bg-red-800">{second}</div>
         </div>
       </div>
     </div>
