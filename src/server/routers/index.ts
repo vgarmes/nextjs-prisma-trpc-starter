76bd51c05360fbe3a371554ab9f2ffde1951e165
Victor Garcia Mestre <vgmestre@gmail.com>
2022-01-20 22:40:00 +0100

    prisma schema and resolvers

--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -1,18 +1,39 @@
 import * as trpc from '@trpc/server';
 import { z } from 'zod';
+import { getOptionsForVote } from '@/utils/getRandomPokemon';
+import { prisma } from 'db';
 
-export const appRouter = trpc.router().query('hello', {
-  input: z
-    .object({
-      text: z.string().nullish(),
-    })
-    .nullish(),
-  resolve({ input }) {
-    return {
-      greeting: `hello ${input?.text ?? 'world'}`,
-    };
-  },
-});
+export const appRouter = trpc
+  .router()
+  .query('get-pokemon-pair', {
+    async resolve() {
+      const [first, second] = getOptionsForVote();
+
+      const bothPokemon = await prisma.pokemon.findMany({
+        where: { id: { in: [first, second] } },
+      });
+
+      if (bothPokemon.length !== 2)
+        throw new Error('Failed to find two Pokemon');
+
+      return { firstPokemon: bothPokemon[0], secondPokemon: bothPokemon[1] };
+    },
+  })
+  .mutation('cast-vote', {
+    input: z.object({
+      votedFor: z.number(),
+      votedAgainst: z.number(),
+    }),
+    async resolve({ input }) {
+      const voteInDb = await prisma.vote.create({
+        data: {
+          votedAgainstId: input.votedAgainst,
+          votedForId: input.votedFor,
+        },
+      });
+      return { success: true, vote: voteInDb };
+    },
+  });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
