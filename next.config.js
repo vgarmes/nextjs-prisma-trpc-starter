939453579e84a10c34671e853e1752642a45ab70
Victor Garcia Mestre <vgmestre@gmail.com>
2022-01-22 16:23:28 +0100

    add source for images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+};
+
+const { withPlausibleProxy } = require('next-plausible');
+
+module.exports = withPlausibleProxy()({
+  images: {
+    domains: ['raw.githubusercontent.com'],
+    minimumCacheTTL: 6000000,
+  },
+});
