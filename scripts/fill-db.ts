d6c1d6c659fce05b56a600090470f505cf9977f2
Victor Garcia Mestre <vgmestre@gmail.com>
2022-01-22 16:25:29 +0100

    script to fill db

---
+++ scripts/fill-db.ts
@@ -0,0 +1,25 @@
+import { PokemonClient } from 'pokenode-ts';
+import { prisma } from '../db';
+import { MAX_DEX_ID } from '../src/constants';
+
+const doBackfill = async () => {
+  const pokeApi = new PokemonClient();
+
+  const allPokemon = await pokeApi.listPokemons(0, MAX_DEX_ID);
+
+  const formattedPokemon = allPokemon.results.map((p, index) => ({
+    id: index + 1,
+    name: (p as { name: string }).name,
+    spriteUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+      index + 1
+    }.png`,
+  }));
+
+  const creation = await prisma.pokemon.createMany({
+    data: formattedPokemon,
+  });
+
+  console.log('Creation?', creation);
+};
+
+doBackfill();
